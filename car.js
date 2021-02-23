ebca5833ac74f1460b429d0c652ffc7c21070a58
Kevin Le <kevinle108@gmail.com>
2021-02-23 13:24:41 -0500

    pullapart functions for location and make

---
+++ car.js
@@ -0,0 +1,41 @@
+// GET - https://enterpriseservice.pullapart.com/Location
+// GET - https://inventoryservice.pullapart.com/Make/
+// GET - https://inventoryservice.pullapart.com/Model?makeID={{MAKEID}}
+// POST - https://inventoryservice.pullapart.com/Vehicle/Search
+const locationURL = 'https://enterpriseservice.pullapart.com/Location';
+const makeURL = 'https://inventoryservice.pullapart.com/Make/';
+
+const https = require('https');
+
+
+// getLocationData(locationURL);
+getMakeData(makeURL);
+
+function getLocationData(locationURL) {
+    https.get('https://enterpriseservice.pullapart.com/Location', response => {
+        console.log(response.statusCode);
+        let body = '';
+        response.on('data', data => body += data.toString());
+        response.on('end', () => {
+            const data = JSON.parse(body);
+            data.forEach(location => {
+                console.log(`${location.locationID}: ${location.locationName}`);
+            });
+        });
+    })
+}
+
+function getMakeData(makeURL) {
+    https.get('https://inventoryservice.pullapart.com/Make/', response => {
+        console.log(response.statusCode);
+        let body = '';
+        response.on('data', data => body += data.toString());
+        response.on('end', () => {
+            const data = JSON.parse(body);            
+            data.forEach(make => {
+                console.log(`${make.makeID}: ${make.makeName}`);
+            });
+        });
+    })
+}
+
